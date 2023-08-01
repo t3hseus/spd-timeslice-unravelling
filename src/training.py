c61d54e70808aa7f78ded56388ceb407b73dce48
maxim <maksppap123@mail.ru>
2023-08-02 02:30:07 +0300

    deleted metric calcualtion logic

--- src/training.py
+++ src/training.py
@@ -13,23 +13,12 @@ from pytorch_metric_learning import (
     miners,
 )
 
+from sklearn.base import ClusterMixin
 from sklearn.cluster import KMeans
-from sklearn.metrics import (
-    calinski_harabasz_score,
-    davies_bouldin_score,
-    silhouette_score,
-    accuracy_score,
-    precision_score,
-    recall_score,
-    f1_score,
-)
 
-from collections import defaultdict
-from functools import partial
-import numpy as np
+from typing import Type, Any, Dict
 
-from typing import Dict
-
+from .metrics import ClusterMetric, Clustering
 from .visualization import draw_embeddings
 
 
@@ -58,10 +47,11 @@ class TripletTracksEmbedder(pl.LightningModule):
         distance: DistanceType = DistanceType.cosine_similarity,
         learning_rate: float = 1e-4,
         weight_decay: float = 1e-2, # default AdamW param
-        umapper: Optional[UMAP] = None
+        umapper: Optional[UMAP] = None,
+        clustering_algorithm: Optional[Any] = None,
     ):
         super().__init__()
-        self.save_hyperparameters(ignore=['model', 'umapper'])
+        self.save_hyperparameters(ignore=['model', 'umapper', 'clustering_algorithm'])
 
         if umapper is None:
             # configure default one
@@ -75,6 +65,13 @@ class TripletTracksEmbedder(pl.LightningModule):
             distance=self._distance,
             reducer=reducers.ThresholdReducer(low=0)
         )
+
+        if clustering_algorithm is None:
+            clustering_algorithm = KMeans(n_clusters=40, random_state=42, n_init=10)
+        self.clustering = Clustering(clustering_algorithm)
+
+        self.cluster_metric = ClusterMetric()
+
         self.learning_rate = learning_rate
         self.weight_decay = weight_decay
         self.triplet_miner = miners.TripletMarginMiner(
@@ -108,112 +105,14 @@ class TripletTracksEmbedder(pl.LightningModule):
         )
         return loss
 
-    def compute_metrics(self, embeddings: torch.Tensor, labels: torch.Tensor, clusters: np.ndarray) -> Dict[str, float]:
-        """
-        Compute various internal and external clustering metrics.
-
-        Parameters
-        ----------
-        embeddings : torch.Tensor
-            The embeddings on which clustering is performed.
-        labels : torch.Tensor
-            The actual labels for the embeddings.
-        clusters : np.ndarray
-            The clusters assignment from clustering algorithm.
-
-        Returns
-        -------
-        Dict[str, float]
-            A dictionary containing the computed metrics.
-        """
-
-        metrics = {}
-        averages = ['macro', 'micro', 'weighted']
-
-        metrics_functions = {
-            'internal': {
-                'silhouette_score': silhouette_score,
-                'davies_bouldin_score': davies_bouldin_score,
-                'calinski_harabasz_score': calinski_harabasz_score,
-            },
-            'external': {
-                'accuracy_score': accuracy_score,
-                **{
-                    f'{average}_{metric}': partial(metric_func, average=average, zero_division=0)
-                    for metric, metric_func in {
-                        'precision_score': precision_score,
-                        'recall_score': recall_score,
-                        'f1_score': f1_score,
-                    }.items()
-                    for average in averages
-                }
-            }
-        }
-
-        embeddings = embeddings.cpu().detach().numpy()
-        labels = labels.cpu().numpy()
-        for metric_name, metric_function in metrics_functions['internal'].items():
-            metrics['int_'+metric_name] = metric_function(embeddings, labels)
-
-        for metric_name, metric_function in metrics_functions['external'].items():
-            metrics['ext_'+metric_name] = metric_function(labels, clusters)
-
-        return metrics
-
-    def clustering(self, embeddings: np.ndarray, labels: np.ndarray) -> np.ndarray:
-        """
-        Perform clustering on the given embeddings using KMeans and map the labels according to maximum set intersection.
-
-        Parameters
-        ----------
-        embeddings : np.ndarray
-            The embeddings on which clustering is to be performed.
-        labels : np.ndarray
-            The actual labels for the given embeddings.
-
-        Returns
-        -------
-        np.ndarray
-            The labels predicted by KMeans mapped according to maximum intersection with actual labels.
-        """
-
-        kmeans = KMeans(n_clusters=40, random_state=0, n_init=10).fit(embeddings)
-        cluster_assignments = kmeans.labels_
-
-        labels_dict = defaultdict(set)
-        cluster_dict = defaultdict(set)
-
-        # add indices to sets for each label
-        for i, val in enumerate(labels.tolist()):
-            labels_dict[val].add(i)
-
-        for i, val in enumerate(cluster_assignments.tolist()):
-            cluster_dict[val].add(i)
-
-        # calculate intersections and link clusters and events
-        cluster_evt_dict = {}
-        for cluster, cluster_set in cluster_dict.items():
-            max_intersect = 0
-            max_evt = None
-            for evt, evt_set in labels_dict.items():
-                intersect = len(cluster_set & evt_set)
-                if intersect > max_intersect:
-                    max_intersect = intersect
-                    max_evt = evt
-            cluster_evt_dict[cluster] = max_evt
-
-        vfunc = np.vectorize(cluster_evt_dict.get)
-
-        return vfunc(cluster_assignments)
-
     def validation_step(self, batch, batch_idx):
         loss, embeddings = self._forward_batch(batch, return_embeddings=True)
 
         emb_np = embeddings.cpu().detach().numpy()
         evt_ids_np = batch[1].cpu().numpy()
 
-        cluster_preds = self.clustering(emb_np, evt_ids_np)
-        metrics = self.compute_metrics(embeddings, batch[1], cluster_preds)
+        cluster_preds = self.clustering(embeddings, batch[1])
+        metrics = self.cluster_metric(embeddings, batch[1], cluster_preds)
 
         self.validation_step_outputs.append({
             "embeddings": emb_np,
@@ -244,11 +143,9 @@ class TripletTracksEmbedder(pl.LightningModule):
             split_name="validation"
         )
 
-        all_metrics = [output["metrics"] for output in self.validation_step_outputs]
-        avg_metrics = {
-            metric: sum(metrics[metric] for metrics in all_metrics) / len(all_metrics)
-            for metric in all_metrics[0]
-        }
+        avg_metrics = self.cluster_metric.compute()
+        self.cluster_metric.reset()
+        self.clustering.reset()
 
         self.logger.experiment.add_figure(
             "Embeddings visualization", plt_obj.gcf(), self.current_epoch
@@ -261,7 +158,7 @@ class TripletTracksEmbedder(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(
-            self.parameters(), 
+            self.parameters(),
             lr=self.learning_rate,
             weight_decay=self.weight_decay
         )
