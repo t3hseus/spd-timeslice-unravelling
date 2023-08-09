9cfebc99ab174d5f726a39b1531e8ecb9d356f0d
maxim <maksppap123@mail.ru>
2023-08-09 19:41:31 +0300

    implemented torchmetrics logic

--- src/metrics.py
+++ src/metrics.py
@@ -3,21 +3,23 @@ import gin
 from torchmetrics import Metric
 from sklearn.metrics import (
     silhouette_score, davies_bouldin_score, calinski_harabasz_score,
-    accuracy_score, f1_score, precision_score, recall_score
+    accuracy_score, f1_score, precision_score, recall_score,
 )
 
 class BaseScoreMetric(Metric):
     def __init__(self, score_func, **kwargs):
         super().__init__()
-        self.score = 0
+        self.score = []
         self.score_func = score_func
         self.score_func_kwargs = kwargs
 
     def update(self, *args, **kwargs):
-        self.score = self.score_func(*args, **kwargs, **self.score_func_kwargs)
+        self.score.append(self.score_func(*args, **kwargs, **self.score_func_kwargs))
 
     def compute(self):
-        return self.score
+        mean_score = sum(self.score) / len(self.score)
+        self.score = []
+        return mean_score
 
 @gin.configurable
 class SilhouetteScoreMetric(BaseScoreMetric):
@@ -41,15 +43,15 @@ class AccuracyScoreMetric(BaseScoreMetric):
 
 @gin.configurable
 class F1ScoreMetric(BaseScoreMetric):
-    def __init__(self, average='micro'):
-        super().__init__(f1_score, average=average)
+    def __init__(self, average='macro'):
+        super().__init__(f1_score, average=average, zero_division=0)
 
 @gin.configurable
 class PrecisionScoreMetric(BaseScoreMetric):
-    def __init__(self, average='micro'):
-        super().__init__(precision_score, average=average)
+    def __init__(self, average='macro'):
+        super().__init__(precision_score, average=average, zero_division=0)
 
 @gin.configurable
 class RecallScoreMetric(BaseScoreMetric):
-    def __init__(self, average='micro'):
-        super().__init__(recall_score, average=average)
+    def __init__(self, average='macro'):
+        super().__init__(recall_score, average=average, zero_division=0)
--- src/training.py
+++ src/training.py
@@ -21,53 +21,8 @@ from collections import defaultdict
 from typing import Optional, Type, Any, Dict
 
 from .metrics import *
+from .clustering import *
 from .visualization import draw_embeddings
-
-
-@gin.configurable(allowlist=None)
-def clustering_algorithm(module: str, class_: str, **kwargs: dict):
-    module = importlib.import_module(module)
-    cls = getattr(module, class_)
-    return cls(**kwargs)
-
-class Clustering:
-    def __init__(self, clustering_algorithm):
-        self.clustering_algorithm = clustering_algorithm
-
-    def compute_clusters(self, embeddings: np.ndarray):
-        cluster_assignments = self.clustering_algorithm.fit_predict(embeddings)
-        return cluster_assignments
-
-    def link_clusters(self, cluster_assignments: np.ndarray, labels: np.ndarray):
-
-        labels_dict = defaultdict(set)
-        cluster_dict = defaultdict(set)
-
-        # add indices to sets for each label
-        for i, val in enumerate(labels.tolist()):
-            labels_dict[val].add(i)
-
-        for i, val in enumerate(cluster_assignments.tolist()):
-            cluster_dict[val].add(i)
-
-        # calculate intersections and link clusters and events
-        cluster_evt_dict = {}
-        for cluster, cluster_set in cluster_dict.items():
-            max_intersect = 0
-            max_evt = None
-            for evt, evt_set in labels_dict.items():
-                intersect = len(cluster_set & evt_set)
-                if intersect > max_intersect:
-                    max_intersect = intersect
-                    max_evt = evt
-            cluster_evt_dict[cluster] = max_evt
-
-        vfunc = np.vectorize(cluster_evt_dict.get)
-        return vfunc(cluster_assignments)
-
-    def cluster_and_link(self, embeddings: np.ndarray, labels: np.ndarray):
-        cluster_assignments = self.compute_clusters(embeddings)
-        return self.link_clusters(cluster_assignments, labels)
 
 
 @gin.constants_from_enum
@@ -117,6 +72,7 @@ class TripletTracksEmbedder(pl.LightningModule):
 
         if clustering_algorithm is None:
             clustering_algorithm = KMeans(n_clusters=40, random_state=42, n_init=10)
+
         self.clustering = Clustering(clustering_algorithm)
 
         self.metrics = metrics
@@ -164,19 +120,22 @@ class TripletTracksEmbedder(pl.LightningModule):
         metrics_results = {}
 
         for metric in self.metrics:
-            if isinstance(metric, (F1ScoreMetric, PrecisionScoreMetric, RecallScoreMetric, AccuracyScoreMetric)):
+            if isinstance(metric, (
+                F1ScoreMetric,
+                PrecisionScoreMetric,
+                RecallScoreMetric,
+                AccuracyScoreMetric
+            )):
                 metric.update(evt_ids_np, cluster_preds)
             elif isinstance(metric, BaseScoreMetric):
                 metric.update(emb_np, cluster_preds)
-
-            metric_name = type(metric).__name__
-            self.epoch_metrics[metric_name].append(metric.compute())
 
         self.validation_step_outputs.append({
             "embeddings": emb_np,
             "event_ids": evt_ids_np,
             "metrics": metrics_results
         })
+
         self.log_dict(
             {
                 "val_loss": loss,
@@ -190,8 +149,13 @@ class TripletTracksEmbedder(pl.LightningModule):
         sample_idx = 0
         sample_for_visualization = self.validation_step_outputs[sample_idx]
 
-        avg_metrics = {name: torch.mean(torch.tensor(values)) for name, values in self.epoch_metrics.items()}
-        self.epoch_metrics.clear()
+        for metric in self.metrics:
+            metric_name = type(metric).__name__
+            self.logger.experiment.add_scalar(
+                f'{metric_name}',
+                metric.compute(),
+                self.current_epoch
+            )
 
         umap_embeddings = self.umapper.fit_transform(
             sample_for_visualization["embeddings"]
@@ -207,9 +171,6 @@ class TripletTracksEmbedder(pl.LightningModule):
         self.logger.experiment.add_figure(
             "Embeddings visualization", plt_obj.gcf(), self.current_epoch
         )
-
-        for metric, value in avg_metrics.items():
-            self.logger.experiment.add_scalar(f'{metric}', value, self.current_epoch)
 
         self.validation_step_outputs.clear()  # free memory
 
