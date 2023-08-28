769b0cd06a5b4dfd4bdf7840e09f3fc4775908c2
maxim <maksppap123@mail.ru>
2023-08-28 09:05:05 +0300

    fix path and save df with time distr

--- eval_check.py
+++ eval_check.py
@@ -1,10 +1,14 @@
+import warnings
+warnings.filterwarnings("ignore", message=".*The 'nopython' keyword.*")
+
 import src.utils
 from src.eval import ModelEvaluator
 import os
 import json
 
+
 if __name__ == "__main__":
-    base_path = "../experiment_logs/TrackEmbedder/" # set your base path here
+    base_path = "./experiment_logs/TrackEmbedder/" # set your base path here
     run_name = "version_0/" # set your run name here
     cfg_path = 'train_config.cfg'
     run_path = os.path.join(base_path, run_name, cfg_path)
@@ -17,14 +21,16 @@ if __name__ == "__main__":
         "cfg_path": cfg_path,
         "BATCH_SIZE": 1,
         "n_samples": 10,
-        "map_location": 'mps' # set your device here
+        "map_location": 'cpu' # set your device here
     }
 
     evaluator = ModelEvaluator(config, parsed_gin)
     # evaluator.visualize_embeddings(num_samples=6) # not implemented
-    result = evaluator.evaluate()
+    result, df_time = evaluator.evaluate()
 
     print(result)
+
+    df_time.to_csv(os.path.join(base_path, run_name, 'eval_time.csv'))
 
     with open(os.path.join(base_path, run_name, 'eval_res.json'), 'w') as f:
         json.dump(result, f)
